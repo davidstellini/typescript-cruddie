99c76e0129643005bad938b59a33d1d6674e155c
dstellini <davste93@gmail.com>
2016-03-01 10:07:50 +0100

    Fixed exports

--- lib/classes/helper/DefaultApiParser.d.ts
+++
@@ -1,7 +0,0 @@
-import { ApiItemParser } from "./ApiParser";
-import { Model } from "../../interfaces/model/Model";
-import { ModelFactory } from "../../interfaces/model/modelFactory";
-export declare class DefaultApiParser<T extends Model> extends ApiItemParser<T> {
-    factory: ModelFactory<T>;
-    constructor(factory: ModelFactory<T>);
-}
