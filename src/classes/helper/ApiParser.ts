2cb8238a92186cb8b95e3c30ad066c34b12ca2ed
David <dstellini@nxpay.eu>
2016-05-16 17:41:09 +0200

    debugging

--- src/classes/helper/ApiParser.ts
+++ src/classes/helper/ApiParser.ts
@@ -4,6 +4,7 @@ import {List} from "./List";
 export class ApiParser {
 
   static Parse<T>(objType: { new(): T; }, json: any) {
+    debugger;
     const newObj = new objType();
     const relationships = objType["relationships"] || {};
 
@@ -27,6 +28,7 @@ export class ApiParser {
 }
 
 static ParseList<T>(objType: { new(): T; }, jsonString: string) : List<T>{
+  debugger;
   var json = JSON.parse(jsonString);
   var items : List<T>  = new List<T>();
   if (!Array.isArray(json)){
